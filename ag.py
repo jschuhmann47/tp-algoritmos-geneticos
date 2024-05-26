eaf519b77b4a03d878172646a20a6686f746dda4
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-05-26 11:26:06 -0300

    fix funcion cromosoma

---
+++ ag.py
@@ -0,0 +1,60 @@
+from cromosoma import generar_cromosoma
+from fixture import calcular_aptitud
+import pygad
+
+def fitness_func(ga_instance, solution, solution_idx):
+    return calcular_aptitud(solution)
+
+fitness_function = fitness_func
+
+def on_start(ga_instance):
+    print("on_start()")
+
+def on_fitness(ga_instance, population_fitness):
+    print("on_fitness()")
+
+def on_parents(ga_instance, selected_parents):
+    print("on_parents()")
+
+def on_crossover(ga_instance, offspring_crossover):
+    print("on_crossover()")
+
+def on_mutation(ga_instance, offspring_mutation):
+    print("on_mutation()")
+
+def on_generation(ga_instance):
+    print("on_generation()")
+
+def on_stop(ga_instance, last_population_fitness):
+    print("on_stop()")
+
+poblacion_inicial_custom = []
+for i in range(20):
+    poblacion_inicial_custom.append(generar_cromosoma())
+
+ga_instance = pygad.GA(num_generations=5,
+                       num_parents_mating=5,
+                       fitness_func=fitness_function,
+                       #sol_per_pop=10,
+                       #num_genes=len(function_inputs),
+                       initial_population=poblacion_inicial_custom,
+                       on_start=on_start,
+                       on_fitness=on_fitness,
+                       on_parents=on_parents,
+                       on_crossover=on_crossover,
+                       on_mutation=on_mutation,
+                       on_generation=on_generation,
+                       on_stop=on_stop)
+
+ga_instance.run()
+
+# ga_instance.plot_fitness()
+
+solution, solution_fitness, solution_idx = ga_instance.best_solution(ga_instance.last_generation_fitness)
+print(f"Parameters of the best solution : {solution}")
+print(f"Fitness value of the best solution = {solution_fitness}")
+print(f"Index of the best solution : {solution_idx}")
+
+
+if ga_instance.best_solution_generation != -1:
+    print(f"Best fitness value reached after {ga_instance.best_solution_generation} generations.")
--- cromosoma.py
+++ cromosoma.py
@@ -6,21 +6,23 @@ def generar_cromosoma():
     for _ in range(19):  # 19 fechas
         fecha = []
         for _ in range(10):  # 10 partidos por fecha
-            equipo_local = random.choice(equipos.values())
-            equipo_visitante = random.choice(equipos.values())
+            equipo_local = random.choice(list(equipos.values()))
+            equipo_visitante = random.choice(list(equipos.values()))
             dia_partido = random.randint(0, 3)  # Se elige aleatoriamente el día del partido
             partido = (equipo_local, equipo_visitante, dia_partido)
             fecha.append(partido)
         cromosoma.append(fecha)
-    return cromosoma
+    return sum(sum(cromosoma, []), ()) # Se concatenan los resultados para pasárselos a la lib
 
 def decodificar_cromosoma(cromosoma):
     fixture = []
-    for fecha in cromosoma:
+    for i in range(19):
+        fecha = cromosoma[i * 30 : (i + 1) * 30]
         partidos_fecha = []
-        for partido in fecha:
-            equipo_local = [equipo for equipo, indice in equipos.items() if indice == equipos[partido[0]]][0]
-            equipo_visitante = [equipo for equipo, indice in equipos.items() if indice == equipos[partido[1]]][0]
+        for j in range(10):
+            partido = fecha[j * 3 : (j + 1) * 3]
+            equipo_local = [equipo for equipo, indice in equipos.items() if indice == partido[0]][0]
+            equipo_visitante = [equipo for equipo, indice in equipos.items() if indice == partido[1]][0]
             dia_partido = [dia for dia, indice in dias.items() if indice == partido[2]][0]
             partidos_fecha.append((equipo_local, equipo_visitante, dia_partido))
         fixture.append(partidos_fecha)
