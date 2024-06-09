d18df2bca9cab1fc5d373732f654190d72937732
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-06-09 11:46:41 -0300

    add output of 5 runs

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # TP Algoritmos Genéticos
 
 [Framework de AG](https://github.com/ahmedfgad/GeneticAlgorithmPython) a utilizar
+
+## Requisitos
+
+Tener instalado python y pip
+
+## Instrucciones
+
+Instalar la biblioteca:
+
+```bash
+pip install pygad
+```
+
+Correr con:
+
+```bash
+python ag.py
+```
--- ag.py
+++ ag.py
@@ -7,32 +7,20 @@ def fitness_func(ga_instance, solution, solution_idx):
 
 fitness_function = fitness_func
 
-def on_fitness(ga_instance, population_fitness):
-    print(ga_instance.generations_completed)
-
 poblacion_inicial_custom = []
 for i in range(50):
     poblacion_inicial_custom.append(generar_cromosoma())
 
+corrida = "5"
 ga_instance = pygad.GA(num_generations=1000,
-                       num_parents_mating=25,
+                       num_parents_mating=40,
+                       parent_selection_type="rws",
+                       crossover_probability=0.85,
+                       mutation_probability=0.15,
                        fitness_func=fitness_function,
                        gene_type=int,
                        initial_population=poblacion_inicial_custom,
                        gene_space=[{"low": 0, "high": 19}, {"low": 0, "high": 19}, {"low": 0, "high": 3}] * 190,
-                       parent_selection_type="sss",
-                       crossover_probability=0.4,
-                       mutation_probability=0.1,
-                    #    parallel_processing=["process", 4],
-                    #    on_start=on_start,
-                    #    on_fitness=on_fitness,
-                    #    on_parents=on_parents,
-                    #    on_crossover=on_crossover,
-                    #    on_mutation=on_mutation,
-                    #    on_generation=on_generation,
-                    #    on_stop=on_stop,
-                       keep_parents=5,
-                       crossover_type="two_points",
                        save_best_solutions=True)
 
 ga_instance.run()
@@ -41,12 +29,14 @@ ga_instance.plot_fitness()
 
 solution, solution_fitness, solution_idx = ga_instance.best_solution(ga_instance.last_generation_fitness)
 solution = decodificar_cromosoma(solution)
-print(f"Parameters of the best solution : {solution}")
-print(f"Fitness value of the best solution = {solution_fitness}")
-print(f"Index of the best solution : {solution_idx}")
 
 soluciones_por_gen = ga_instance.best_solutions
-print(soluciones_por_gen)
-
+joined = '\n'.join(', '.join(map(str, row)) for row in soluciones_por_gen)
+file1 = open("output" + corrida + ".csv", "w")
+file1.write(joined)
+file1.write('\nEND\n')
+file1.write(f"Parameters of the best solution : {solution}\n")
+file1.write(f"Fitness value of the best solution = {solution_fitness}\n")
 if ga_instance.best_solution_generation != -1:
-    print(f"Best fitness value reached after {ga_instance.best_solution_generation} generations.")
+    file1.write(f"Best fitness value reached after {ga_instance.best_solution_generation} generations.\n")
+file1.close()
